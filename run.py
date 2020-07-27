6d074b43fcc4090b17bfd70b874e757cc40a4dd0
Luiz Ribeiro <luizribeiro@gmail.com>
2020-07-26 20:12:45 -0400

    Made run.py use tdklambda module

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
-from serial import Serial
+from tdklambda import TDKLambdaPSU
 
-serial = Serial("/dev/ttyUSB0", 9600, xonxoff=True)
-serial.write(b":ADR01;")
-print(f"Returned: {serial.readline()}")
+with TDKLambdaPSU("/dev/ttyUSB0", 9600) as psu:
+    psu.set_voltage(0.0)
+    psu.set_current(0.0)
+    print(f"Model: {psu.get_model()}")
+    print(f"Software Version: {psu.get_software_version()}")
+    print(f"Set Voltage: {psu.get_set_voltage():.3f} V")
+    print(f"Actual Voltage: {psu.get_actual_voltage():.3f} V")
+    print(f"Set Current: {psu.get_set_current():.3f} A")
+    print(f"Actual Current: {psu.get_actual_current():.3f} A")
+    print(f"Output: {'ON' if psu.is_output_on() else 'OFF'}")
+    psu.close()
