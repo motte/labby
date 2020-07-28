b63f9be28792ce8c46308e55ae9d813717c25cc5
Luiz Ribeiro <luizribeiro@gmail.com>
2020-07-27 23:24:51 -0400

    Setup PSU abstract base class with a few methods

--- labctl/hw/core.py
+++ labctl/hw/core.py
@@ -1,4 +1,4 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from enum import Enum
 
 
@@ -8,4 +8,38 @@ class PSUMode(Enum):
 
 
 class PSU(ABC):
-    pass
+    @abstractmethod
+    def get_mode(self) -> PSUMode:
+        ...
+
+    @abstractmethod
+    def open(self) -> None:
+        ...
+
+    @abstractmethod
+    def close(self) -> None:
+        ...
+
+    @abstractmethod
+    def get_set_voltage(self) -> float:
+        ...
+
+    @abstractmethod
+    def get_actual_voltage(self) -> float:
+        ...
+
+    @abstractmethod
+    def get_set_current(self) -> float:
+        ...
+
+    @abstractmethod
+    def get_actual_current(self) -> float:
+        ...
+
+    @abstractmethod
+    def set_voltage(self, voltage: float) -> None:
+        ...
+
+    @abstractmethod
+    def set_current(self, current: float) -> None:
+        ...
