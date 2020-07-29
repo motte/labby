5592c7dea125eee1d854480586149a11ab9c6213
Luiz Ribeiro <luizribeiro@gmail.com>
2020-07-29 10:32:39 -0400

    Added typing to labctl.tests.utils

--- labctl/tests/utils.py
+++ labctl/tests/utils.py
@@ -1,10 +1,11 @@
 import io
 import sys
 from contextlib import contextmanager, redirect_stderr, redirect_stdout
+from typing import Iterator, Tuple
 
 
 @contextmanager
-def captured_output():
+def captured_output() -> Iterator[Tuple[io.StringIO, io.StringIO]]:
     stdout, stderr = io.StringIO(), io.StringIO()
     with redirect_stdout(stdout), redirect_stderr(stderr):
         yield stdout, stderr
