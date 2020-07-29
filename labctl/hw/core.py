ab205708e4014757bcdba4caad7490fc9d8d615c
Luiz Ribeiro <luizribeiro@gmail.com>
2020-07-29 10:14:58 -0400

    Ignore abstract methods from code coverage

--- labctl/hw/core.py
+++ labctl/hw/core.py
@@ -10,47 +10,47 @@ class PSUMode(Enum):
 class PSU(ABC):
     @abstractmethod
     def get_mode(self) -> PSUMode:
-        ...
+        raise NotImplementedError
 
     @abstractmethod
     def open(self) -> None:
-        ...
+        raise NotImplementedError
 
     @abstractmethod
     def close(self) -> None:
-        ...
+        raise NotImplementedError
 
     @abstractmethod
     def is_output_on(self) -> bool:
-        ...
+        raise NotImplementedError
 
     @abstractmethod
     def set_output_on(self, is_on: bool) -> None:
-        ...
+        raise NotImplementedError
 
     @abstractmethod
     def get_target_voltage(self) -> float:
-        ...
+        raise NotImplementedError
 
     @abstractmethod
     def get_actual_voltage(self) -> float:
-        ...
+        raise NotImplementedError
 
     @abstractmethod
     def get_target_current(self) -> float:
-        ...
+        raise NotImplementedError
 
     @abstractmethod
     def get_actual_current(self) -> float:
-        ...
+        raise NotImplementedError
 
     @abstractmethod
     def set_target_voltage(self, voltage: float) -> None:
-        ...
+        raise NotImplementedError
 
     @abstractmethod
     def set_target_current(self, current: float) -> None:
-        ...
+        raise NotImplementedError
 
 
 class HardwareIOException(Exception):
