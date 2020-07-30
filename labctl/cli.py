4b3bd21a3489b74c0e2a967257467a66d271fd42
Luiz Ribeiro <luizribeiro@gmail.com>
2020-07-29 21:55:41 -0400

    Allow commands to be passed into cli

--- labctl/cli.py
+++ labctl/cli.py
@@ -2,10 +2,16 @@ from tap import Tap
 
 
 class ArgumentParser(Tap):
-    hello: bool = False
+    command: str = "help"
+
+    def add_arguments(self) -> None:
+        self.add_argument("command")
 
 
 def main() -> None:
     args = ArgumentParser().parse_args()
-    if args.hello:
-        print("Hello world")
+    if args.command == "help":
+        args.print_help()
+        return
+
+    print("Hello world")
--- labctl/tests/test_cli.py
+++ labctl/tests/test_cli.py
@@ -6,11 +6,23 @@ from labctl.tests.utils import captured_output, cli_arguments
 
 
 class CommandLineTest(TestCase):
-    def main(self, arguments: List[str]) -> Tuple[str, str]:
+    def main(self, arguments: List[str]) -> Tuple[int, str, str]:
+        rc = 0
         with cli_arguments(arguments), captured_output() as (stdout, stderr):
-            cli.main()
-        return (stdout.getvalue(), stderr.getvalue())
+            try:
+                cli.main()
+            except SystemExit as ex:
+                rc = ex.code
+        return (rc, stdout.getvalue(), stderr.getvalue())
 
     def test_easter_egg(self):
-        (stdout, stderr) = self.main(["--hello"])
+        (rc, stdout, _stderr) = self.main(["hello"])
+        self.assertEqual(rc, 0)
         self.assertEqual(stdout, "Hello world\n")
+
+    def test_command_is_required(self):
+        (rc, stdout, stderr) = self.main([])
+        self.assertEqual(rc, 2)
+        self.assertTrue(
+            "labctl: error: the following arguments are required: command\n" in stderr
+        )
