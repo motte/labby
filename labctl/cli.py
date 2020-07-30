d65c54c5e3d641c433f37910397ee6a7a92fcc71
Luiz Ribeiro <luizribeiro@gmail.com>
2020-07-29 23:54:41 -0400

    [cli] Command for listing devices

--- labctl/cli.py
+++ labctl/cli.py
@@ -2,9 +2,12 @@ import sys
 from enum import Enum
 from tap import Tap
 
+from labctl.config import Config
+
 
 class Command(Enum):
     HELLO = "hello"
+    DEVICES = "devices"
 
 
 # pyre-ignore[13]: command is unitialized
@@ -13,6 +16,12 @@ class ArgumentParser(Tap):
 
     def add_arguments(self) -> None:
         self.add_argument("command")
+
+
+def list_devices(config: Config) -> None:
+    print("Available Devices:")
+    for device in config.devices:
+        print(f"● {device.__class__}")
 
 
 def main() -> None:
@@ -27,3 +36,10 @@ def main() -> None:
     if command == Command.HELLO:
         print("Hello world")
         return
+
+    with open("labctl.yml", "r") as config_file:
+        config = Config(config_file.read())
+
+    if command == Command.DEVICES:
+        list_devices(config)
+        sys.exit(0)
