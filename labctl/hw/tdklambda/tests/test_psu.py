af983a88f1bc582f819a5df954ff24d0a2996399
Luiz Ribeiro <luizribeiro@gmail.com>
2020-07-30 20:21:33 -0400

    [tdklambda/psu] Added tests for get_target_current

--- labctl/hw/tdklambda/tests/test_psu.py
+++ labctl/hw/tdklambda/tests/test_psu.py
@@ -89,3 +89,11 @@ class ZUPTest(TestCase):
             returned_target_voltage = psu.get_target_voltage()
             self.serial_port_mock.write.assert_called_once_with(b":VOL!;")
             self.assertAlmostEqual(returned_target_voltage, 1.42)
+
+    def test_get_target_current(self) -> None:
+        with tdklambda_psu.ZUP("/dev/ttyUSB0", 9600, address=42) as psu:
+            self.serial_port_mock.reset_mock()
+            self.serial_port_mock.readline.return_value = b"SA0.01\r\n"
+            returned_target_current = psu.get_target_current()
+            self.serial_port_mock.write.assert_called_once_with(b":CUR!;")
+            self.assertAlmostEqual(returned_target_current, 0.01)
