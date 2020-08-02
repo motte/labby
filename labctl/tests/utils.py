e1649dbf57017eb7718f614408b30543a03d910b
Luiz Ribeiro <luizribeiro@gmail.com>
2020-08-02 12:46:18 -0400

    Test utility for patching file contents

--- labctl/tests/utils.py
+++ labctl/tests/utils.py
@@ -1,7 +1,7 @@
 import io
 from contextlib import contextmanager, redirect_stderr, redirect_stdout
-from typing import Iterator, List, Tuple
-from unittest.mock import patch, mock_open, Mock
+from typing import Dict, Iterator, List, Tuple
+from unittest.mock import patch, mock_open, MagicMock, Mock
 
 
 @contextmanager
@@ -17,9 +17,38 @@ def cli_arguments(arguments: List[str]) -> Iterator[None]:
         yield
 
 
+class _OpenMockFileStore:
+    filename_to_mock: Dict[str, MagicMock] = {}
+
+    def register(self, filename: str, contents: str) -> None:
+        assert filename not in self.filename_to_mock.keys()
+        mock = mock_open(read_data=contents)
+        self.filename_to_mock[filename] = mock
+
+    def unregister(self, filename: str) -> None:
+        assert filename in self.filename_to_mock.keys()
+        del self.filename_to_mock[filename]
+
+    def open(self, filename: str, *args, **kwargs) -> MagicMock:
+        return self.filename_to_mock[filename]()
+
+
+OPEN_MOCK_FILE_STORE = _OpenMockFileStore()
+
+
+@contextmanager
+def patch_file_contents(filename: str, contents: str) -> Iterator[None]:
+    OPEN_MOCK_FILE_STORE.register(filename, contents)
+    try:
+        with patch("builtins.open", OPEN_MOCK_FILE_STORE.open):
+            yield
+    finally:
+        OPEN_MOCK_FILE_STORE.unregister(filename)
+
+
 @contextmanager
 def labctl_config(config_contents: str) -> Iterator[None]:
-    with patch("builtins.open", mock_open(read_data=config_contents)):
+    with patch_file_contents("labctl.yml", config_contents):
         yield
 
 
