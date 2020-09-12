39c36c552c2122889583d9adf16334f8b9000c9b
Luiz Ribeiro <luizribeiro@gmail.com>
2020-09-12 12:42:27 -0400

    Fixed CI test runs

--- labby/server/logging.py
+++ labby/server/logging.py
@@ -1,11 +1,18 @@
 import logging
+import os
+
+
+class CustomFileHandler(logging.FileHandler):
+    def __init__(self, filename: str) -> None:
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        logging.FileHandler.__init__(self, filename)
 
 
 logger: logging.Logger = logging.getLogger("labby")
 
 formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
 
-handler = logging.FileHandler(".labby/labby.log")
+handler = CustomFileHandler(".labby/labby.log")
 handler.setFormatter(formatter)
 
 logger.addHandler(handler)
